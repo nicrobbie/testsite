73722ede5c6aacf14e00bc0bcc634bbfed359a7b
nenslen <nicolas.enslen@gmail.com>
2016-05-25 11:56:38 -0600

    Added vdp dealer component

---
+++ client/app/components/vdpDealer/vdpDealer.controller.js
@@ -0,0 +1,10 @@
+export default class vdpDealerController {
+  static get UID(){
+    return "vdpDealerController"
+  }
+
+  /* @ngInject */
+  constructor() {
+    this.title = "I am a vdpDealer component"
+  }
+}
