2eef9630e863805a4e01872a02984157e29ddd34
nenslen <nicolas.enslen@gmail.com>
2016-05-25 11:57:17 -0600

    Added vdp info component

---
+++ client/app/components/vdpInfo/vdpInfo.controller.js
@@ -0,0 +1,10 @@
+export default class vdpInfoController {
+  static get UID(){
+    return "vdpInfoController"
+  }
+
+  /* @ngInject */
+  constructor() {
+    this.title = "I am a vdpInfo component"
+  }
+}
