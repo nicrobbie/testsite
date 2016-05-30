f567d5d48b2c297d2ed5d7ddf2038a6ed928d55e
nenslen <nicolas.enslen@gmail.com>
2016-05-30 16:52:23 -0600

    Get data from cDemo

--- client/app/features/main/main.controller.js
+++ client/app/features/main/main.controller.js
@@ -12,33 +12,36 @@ class mainController {
     this.searchTerm = "";
     this.description = "cDemo Live is a search engine that lets you find products of all kinds, and specializes in New & Used Auto";
     this.products = products;
-    this.productSelected = 1;
-
+    this.productSelected = this.products[0];
+    this.productCategorySelected = -1;
+console.log("This.products: " + this.products[0].name);
     //this.products = products;
     //console.log(ProductService.getProducts().then(response => response.data));
     //console.log(products);
-    this.productCategories = []; //ProductService.getProductCategories(1); //[];
-    //console.log(this.productCategories);
-  	this.$scope.$watch(() => this.productSelected, this.productChange);
+
+    console.log(this.productCategories);
+
+  	//this.$scope.$watch(() => this.productSelected, this.productChange);
   }
 
-  productChange(newVal, oldVal) {
+
+ //productChange(newVal, oldVal) {
+
     //console.log(newVal);
+
+    /*
     return () => {
       console.log(newVal);
       console.log(this.ProductService.getProductCategories(newVal).then(response => response.data.categories));
     }
-    
+    */
+  //}
 
-    //console.log(newVal);
-    //this.productCategories = this.ProductService.getCategories(newVal).then(response => response.data.categories);
-  }
 
   searchChange(newVal) {
   	console.log(newVal);
-      //console.log(this.searchTerm);
-    
   }
+
 
   search() {
   	this.$log.log(this.searchTerm);
