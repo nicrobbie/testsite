8a31c5c372ca68fcfcffb3637adacf35c8d4c7a4
nenslen <nicolas.enslen@gmail.com>
2016-05-31 18:14:47 -0600

    Added socialmedia to the wrong place

---
+++ socialMedia/socialMedia.controller.js
@@ -0,0 +1,10 @@
+export default class socialMediaController {
+  static get UID(){
+    return "socialMediaController"
+  }
+
+  /* @ngInject */
+  constructor() {
+    this.title = "I am a socialMedia component"
+  }
+}
