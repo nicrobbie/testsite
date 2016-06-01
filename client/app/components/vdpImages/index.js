0e412068721fee581490db5b9bdf21ba96825d6c
nenslen <nicolas.enslen@gmail.com>
2016-05-31 18:13:01 -0600

    Pull data from online

--- client/app/components/vdpImages/index.js
+++ client/app/components/vdpImages/index.js
@@ -4,6 +4,7 @@ import './vdpImages.scss';
 
 import angular from 'angular';
 
+
 // Import internal modules
 import controller from './vdpImages.controller';
 import directive from './vdpImages.directive';
@@ -13,8 +14,15 @@ import config from './vdpImages.config';
 
 
 export default angular.module("vdpImages" , [])
-.config(config)
-.run(run)
+	.config(config)
+	.run(run)
+	.service('InfoService', function ($http, $q) {
+
+		this.getInfo = function (id) {
+			return $http.jsonp(`http://live-uat.cdemo.com/jsonp/detail/${id}?callback=JSON_CALLBACK`);
+		};
+
+	})
   .controller(controller.UID, controller)
   .directive("vdpImages", directive)
   .name;
