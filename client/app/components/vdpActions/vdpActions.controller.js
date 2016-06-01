436dbe21de1d59b7c1a100aff8743ebad2de8dfa
nenslen <nicolas.enslen@gmail.com>
2016-06-01 17:12:08 -0600

    Fixed animations

--- client/app/components/vdpActions/vdpActions.controller.js
+++ client/app/components/vdpActions/vdpActions.controller.js
@@ -12,20 +12,27 @@ export default class vdpActionsController {
 
   toggleContent(num) {
 
+    document.getElementById("action1").style.display = "none";
+    document.getElementById("action2").style.display = "none";
+    document.getElementById("action3").style.display = "none";
+
     // Hide/show the correct content area
     if(num == 1) {
+      document.getElementById("action1").style.display = "block";
       this.reserveShow = !this.reserveShow;
       this.tradeShow = false;
       this.testShow = false;
     }
 
     if(num == 2) {
+      document.getElementById("action2").style.display = "block";
       this.reserveShow = false;
       this.tradeShow = !this.tradeShow;
       this.testShow = false;
     }
 
     if(num == 3) {
+      document.getElementById("action3").style.display = "block";
       this.reserveShow = false;
       this.tradeShow = false;
       this.testShow = !this.testShow;
