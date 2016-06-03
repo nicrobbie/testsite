4b170cabc85bdbd3d5b8ecc3879dd8e175037cf8
Robbie <r_eliuk@hotmail.com>
2016-06-03 17:38:03 -0600

    added ng-hide and repeat to make it better and out of controller

--- client/app/components/viewSelect/viewSelect.controller.js
+++ client/app/components/viewSelect/viewSelect.controller.js
@@ -6,21 +6,6 @@ export default class viewSelectController {
   /* @ngInject */
   constructor() {
     this.title = "I am a viewSelect component"
-  }
-
-  /*gridClick(){
-    document.getElementById('view').innerHTML=`<div id="gridView"><grid-view></grid-view></div>`;    
-  }
-  listClick(){
-    document.getElementById('view').innerHTML=`<div id="listView"><list-view></list-view></div>`;    
-  }*/
-  
-  gridClick(){
-    document.getElementById('gridView').style.display="block";    
-  	document.getElementById('listView').style.display="none";  	
-  }
-  listClick(){
-    document.getElementById('listView').style.display="block";
-    document.getElementById('gridView').style.display="none";
+    this.value="grid" //default view set to grid
   }
 }
