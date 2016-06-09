8619cac95f0dc9db3e0f279a4e28a8d3f6a6e7f2
nenslen <nicolas.enslen@gmail.com>
2016-06-09 13:05:37 -0600

    images working

--- client/app/app.js
+++ client/app/app.js
@@ -4,7 +4,7 @@ import './app.scss';
 import angular from 'angular';
 import uirouter from 'angular-ui-router';
 import ngAnimate from 'angular-animate';
-import pagin from 'angular-utils-pagination';
+//import pagin from 'angular-utils-pagination';
 
 // Import base modules
 import config from './app.config';
@@ -31,13 +31,13 @@ import vdpSimilar from './components/vdpSimilar';
 import upArrow from './components/upArrow';
 import header from './components/header';
 import socialMedia from './components/socialMedia';
-import viewSelect from './components/viewSelect';
+//import viewSelect from './components/viewSelect';
 import searchPanel from './components/searchPanel';
 import listView from './components/listView';
 
 
 
-export default angular.module('testsite', [uirouter, ngAnimate, pagin, footer, header, main, records, vdp, filterBank, pagination, gridView, vdpActions, vdpBanner, vdpDealer, vdpImages, vdpInfo, vdpSimilar, searchPanel, listView, socialMedia, upArrow, viewSelect])
+export default angular.module('testsite', [uirouter, ngAnimate, footer, header, main, records, vdp, filterBank, pagination, gridView, vdpActions, vdpBanner, vdpDealer, vdpImages, vdpInfo, vdpSimilar, searchPanel, listView, socialMedia, upArrow])
   .config(config)
   .config(routes)
   .constant("AppConstants", appConstants)
--- client/app/components/vdpSimilar/vdpSimilar.controller.js
+++ client/app/components/vdpSimilar/vdpSimilar.controller.js
@@ -7,45 +7,15 @@ class vdpSimilarController {
   constructor($scope, $http) {
 
   	$scope.similarRecords = [];
-  	$scope.similarRecordsImages = [];
-  	/*
-"http://static.cdemo.com/614234/1024/automobiles-used-2011-chevrolet-camaro-2dr-cpe-2ss-sold-1670635-primary-listing-photo-Image.jpg",
-"http://static.cdemo.com/20151105xvcqqffh/1155416.jpg",
-"http://static.cdemo.com/20151005vijlneoh/2274729.jpg",
-"http://static.cdemo.com/20151002uiwgmjcm/3454632.jpg",
-"http://static.cdemo.com/20151002kghclrux/5021636.jpg",
-"http://static.cdemo.com/20151001bmcfngdw/4043080.jpg",
-"http://static.cdemo.com/20151001iivgtflg/7570941.jpg",
-"http://static.cdemo.com/20150930nubnstbq/5185879.jpg",
-"http://static.cdemo.com/20150813pswleolv/5432610.jpg"];*/
 
 
-  	// Get related vehicle records
+  	// Get related vehicle images
   	for(var i = 0; i < this.item.similar_records.length; i++) {
-      $scope.similarRecords.push($http.jsonp(`http://live-uat.cdemo.com/jsonp/detail/${this.item.similar_records[i]}?callback=JSON_CALLBACK`));
+  		$http.jsonp(`http://live-uat.cdemo.com/jsonp/detail/${this.item.similar_records[i]}?callback=JSON_CALLBACK`)
+  			.success(function(data) {
+  				$scope.similarRecords.push(data[0]);
+  			});
     }
-
-
-
-    this.getItem(this.item.record_id).success(function(data) {
-    	console.log(data);
-    });
-
-
-    
-    angular.element(document).ready(function () {
-
-    	// Get images of related vehicles
-		for(var i = 0; i < $scope.similarRecords.length; i++) {
-			try {
-				$scope.similarRecordsImages.push($scope.similarRecords[i].$$state.value.data[0].primary_photo);
-			} catch(err) {
-				
-			}
-		}
-		
-		console.log($scope.similarRecordsImages);
-    });
   }
 }
 
